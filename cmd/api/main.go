e73939090683ca4c1a306018f381515c216d4400
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-08-22 03:31:53 -0300

    add database connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+
+	"github.com/RivellionCS/TaskForge/internal/database"
+)
 
 func main() {
-	fmt.Println("TaskForge API starting...")
+	ctx := context.Background()
+
+	db, err := database.NewPool(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	log.Println("Connected to PostgreSQL")
 }

0bb8a91256c505a36bf9f9e91290300e325ebbc0
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-08-22 04:02:40 -0300

    connect repository and handler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
 
+	"github.com/RivellionCS/TaskForge/internal/api"
 	"github.com/RivellionCS/TaskForge/internal/database"
+	"github.com/RivellionCS/TaskForge/internal/jobs"
 )
 
 func main() {
@@ -15,6 +18,19 @@ func main() {
 		log.Fatal(err)
 	}
 	defer db.Close()
+
+	jobRepository := jobs.NewRepository(db)
+	jobHandler := api.NewJobHandler(jobRepository)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /jobs", jobHandler.CreateJob)
+
+	log.Println("TaskForge API listening on :8080")
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
 
 	log.Println("Connected to PostgreSQL")
 }

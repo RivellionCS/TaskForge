67b2bbafcc5725412fd866d428f2fa1075c7f222
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-09-15 15:15:48 -0300

    change guest to taskforge

--- internal/queue/rabbitmq.go
+++ internal/queue/rabbitmq.go
@@ -12,7 +12,7 @@ type RabbitMQ struct {
 }
 
 func NewRabbitMQ() (*RabbitMQ, error) {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672")
+	conn, err := amqp.Dial("amqp://taskforge:taskforge@localhost:5672")
 	if err != nil {
 		return nil, fmt.Errorf("Connect to RabbitMQ: %w", err)
 	}

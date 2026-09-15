1552563d7678738df7bf237f5c03bd5536622505
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-09-15 15:33:05 -0300

    remove test

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -30,12 +30,6 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if err := rabbitmq.PublishJob("test-job-123"); err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Test job published to RabbitMQ")
-
 	jobRepository := jobs.NewRepository(db)
 	jobHandler := api.NewJobHandler(jobRepository)
 

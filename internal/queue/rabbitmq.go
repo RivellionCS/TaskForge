1255d1768cdac1b746b6d4b088af81b32077c99e
RianAhmedWork <146366018+RianAhmedWork@users.noreply.github.com>
2026-09-15 15:02:47 -0300

    create rabbitmq connection

---
+++ internal/queue/rabbitmq.go
@@ -0,0 +1,35 @@
+package queue
+
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type RabbitMQ struct {
+	conn *amqp.Connection
+	channel *amqp.Channel
+}
+
+func NewRabbitMQ() (*RabbitMQ, error) {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672")
+	if err != nil {
+		return nil, fmt.Errorf("Connect to RabbitMQ: %w", err)
+	}
+
+	channel, err := conn.Channel()
+	if err != nil {
+		conn.Close()
+		return nil, fmt.Errorf("open RabbitMQ channel: %w", err)
+	}
+
+	return &RabbitMQ{
+		conn: conn,
+		channel: channel,
+	}, nil
+}
+
+func (r *RabbitMQ) Close() {
+	r.channel.Close()
+	r.conn.Close()
+}
